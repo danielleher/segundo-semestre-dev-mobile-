c481cc4700f618c2abcd34ab03216dfda326644f
danielleher <daniellehloura@gmail.com>
2025-11-25 11:35:35 -0300

    atividade 22
    sprite animations

---
+++ entregas/entrega 24 atividade 22/spriteanimations.gd
@@ -0,0 +1,30 @@
+extends Control
+
+@onready var sprite = $AnimatedSprite2D
+@onready var sprite2 = $AnimatedSprite2D2
+@onready var button = $Button
+
+var is_playing = false
+
+func _ready():
+	button.text = "Iniciar"
+	button.pressed.connect(_on_button_pressed)
+
+func _on_button_pressed():
+	_toggle_animation()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept"): # tecla espaço padrão
+		_toggle_animation()
+
+func _toggle_animation():
+	if is_playing:
+		sprite.stop()
+		sprite2.stop()
+		is_playing = false
+		button.text = "Iniciar"
+	else:
+		sprite.play("run")
+		sprite2.play("walk")
+		is_playing = true
+		button.text = "Parar"
